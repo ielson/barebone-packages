a3920529b75a91239cf859aba023744ec5ceb930
ielson <danmascandrade@gmail.com>
2023-07-14 19:47:24 -0300

    added readme to PyPi description

--- setup.py
+++ setup.py
@@ -1,5 +1,10 @@
 from setuptools import setup
 
+# read the contents of your README file
+from pathlib import Path
+this_directory = Path(__file__).parent
+long_description = (this_directory / "README.md").read_text()
+
 setup(
     name='python-barebone',
     version='0.1.0',
@@ -12,6 +17,8 @@ setup(
     install_requires=[],     # like a requirements.txt file,
     setup_requires=['pytest-runner'],  # we can add 'flake8' here to chekc the formatting of our code
     tests_require=['pytest'],
+    long_description=long_description,
+    long_description_content_type='text/markdown',
 
     classifiers=[
         'Development Status :: 1 - Planning',
