8d38a531fda834b3f3823fd1cbed91f51da1ab00
ielson <danmascandrade@gmail.com>
2023-07-16 18:16:16 -0300

    changes to folder naming

--- setup.py
+++ setup.py
@@ -2,8 +2,8 @@ from setuptools import setup
 
 # read the contents of your README file
 from pathlib import Path
-this_directory = Path(__file__).parent
-long_description = (this_directory / "README.md").read_text()
+parent_directory = Path(__file__).parent
+long_description = (parent_directory / "README.md").read_text()
 
 setup(
     name='pythonbare',
@@ -12,7 +12,9 @@ setup(
     author='ielson',
     author_email='danmascandrade@gmail.com',
     license='MIT License',
-    packages=['pythonbare'],
+    #packages=['pythonbare.module'],
+    package_dir={'':'pythonbare'},
+    py_modules=['module'],
     install_requires=[],     # like a requirements.txt file,
     setup_requires=['pytest-runner'],  # we can add 'flake8' here to chekc the formatting of our code
     tests_require=['pytest'],
