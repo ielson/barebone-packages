d472c5e4ffc2d3c3cec233403a08785730bd39b0
ielson <danmascandrade@gmail.com>
2023-07-16 16:53:25 -0300

    changes in the model's name - cant have an - in python

--- setup.py
+++ setup.py
@@ -6,13 +6,13 @@ this_directory = Path(__file__).parent
 long_description = (this_directory / "README.md").read_text()
 
 setup(
-    name='python-barebone',
+    name='pythonbare',
     description='An example Python package',
     url='https://github.com/ielson/barebone-packages',
     author='ielson',
     author_email='danmascandrade@gmail.com',
     license='MIT License',
-    packages=['python-barebone'],
+    packages=['pythonbare'],
     install_requires=[],     # like a requirements.txt file,
     setup_requires=['pytest-runner'],  # we can add 'flake8' here to chekc the formatting of our code
     tests_require=['pytest'],
